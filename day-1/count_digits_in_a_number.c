359956ac09c8a2b54f42b2016c1a274c711eca79
Arpit-jaiswal003 <msarpit3@gmail.com>
2026-05-26 13:42:51 +0530

    code

--- day-1/count_digits_in_a_number.c
+++ day-1/count_digits_in_a_number.c
@@ -1,2 +1,17 @@
 #include<stdio.h>
-int main()
+int main(){
+    int number, count=0;
+    printf("enter a number:");
+    scanf("%d",&number);
+    //handle zero
+    if(number==0){
+        count=1;
+    }
+//count digit
+    while(number!=0){
+        number=number/10;
+        count++;
+    }
+    printf("number of digits  is %d", count);
+    return 0;
+}
