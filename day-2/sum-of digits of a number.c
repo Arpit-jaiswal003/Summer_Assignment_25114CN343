66b7e6107bdbd47561df1f6de14f25ea966df678
Arpit-jaiswal003 <msarpit3@gmail.com>
2026-05-29 17:46:21 +0530

    Create sum-of digits of a number.c

---
+++ day-2/sum-of digits of a number.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+    int firstnum,secondnum ,sum=0;
+    printf("enter first number: ");
+    scanf("%d",&firstnum);
+    printf("enter second number:");
+    scanf("%d",&secondnum);    
+    sum=firstnum+secondnum;
+    printf("result=%d",sum);
+    return 0;
+}   
