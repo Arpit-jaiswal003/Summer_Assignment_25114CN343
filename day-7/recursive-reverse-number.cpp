3b7a918783069b15176cfc8b91b944c5494db7da
Arpit-jaiswal003 <msarpit3@gmail.com>
2026-06-07 12:33:32 +0530

    day-7 assignment completed

---
+++ day-7/recursive-reverse-number.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void reverse(int n)
+{
+
+  
+   if (n < 10) {
+      cout<<n;
+      return;
+   }
+
+   else{
+      cout<<n%10;
+      reverse(n/10);
+   }
+}
+
+
+int main()
+{
+   int n;
+   cout<<"Enter a number = ";
+   cin>> n;
+   reverse(n);
+   return 0;
+}
---
+++ day-7/recursive-sum-of-digits.cpp
@@ -0,0 +1,23 @@
+
+#include <bits/stdc++.h> 
+using namespace std; 
+
+
+int sum_of_digit(int n) 
+{ 
+    if (n == 0) 
+    return 0; 
+    return (n % 10 + sum_of_digit(n / 10)); 
+} 
+
+
+int main() 
+{ 
+    int num;
+    cout<<"Enter a number = ";
+    cin>> num;
+
+    int result = sum_of_digit(num); 
+    cout << result << endl; 
+    return 0; 
+}
