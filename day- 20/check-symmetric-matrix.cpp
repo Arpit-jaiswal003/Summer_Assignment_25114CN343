933451c69c4c1f56b1f6f4932e958ef75830537c
Arpit-jaiswal003 <msarpit3@gmail.com>
2026-06-20 17:04:46 +0530

    day-20 assignment completed

---
+++ day- 20/check-symmetric-matrix.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    
+    cout << "Enter the order of the matrix= ";
+    cin >> n;
+
+    int A[50][50];
+
+    cout << "Enter the elements of the matrix=\n";
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> A[i][j];
+        }
+    }
+
+    bool symmetric = true;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (A[i][j] != A[j][i]) {
+                symmetric = false;
+                break;
+            }
+        }
+        if (!symmetric)
+            break;
+    }
+
+    if (symmetric)
+        cout << "The matrix is Symmetric." << endl;
+    else
+        cout << "The matrix is Not Symmetric." << endl;
+
+    return 0;
+}
