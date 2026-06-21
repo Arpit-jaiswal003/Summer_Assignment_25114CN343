4f3b90e857633e7df69d0996c0b8a2dd7054f8b4
Arpit-jaiswal003 <msarpit3@gmail.com>
2026-06-21 22:52:53 +0530

    day-21 assignment completed

---
+++ day- 21/reverse-a-string.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char str[100];
+    
+    cout << "Enter a string= ";
+    cin.getline(str, 100);
+
+    int length = 0;
+
+   
+    while (str[length] != '\0') {
+        length++;
+    }
+
+    for (int i = length - 1; i >= 0; i--) {
+        cout << str[i];
+    }
+
+    cout << endl;
+    return 0;
+}
