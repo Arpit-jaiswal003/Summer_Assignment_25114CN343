65361b00af6a4657080efc846c2d189ed9d73249
Arpit-jaiswal003 <msarpit3@gmail.com>
2026-06-23 22:01:21 +0530

    day-23 assignment uploaded

---
+++ day- 23/first-repeating-character.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string str;
+    
+    cout << "Enter a string= ";
+    getline(cin, str);
+
+    int freq[256] = {0};
+
+  
+    for (int i = 0; i < str.length(); i++) {
+        freq[(int)str[i]]++;
+
+        if (freq[(int)str[i]] > 1) {
+            cout << "First repeating character= " << str[i] << endl;
+            return 0;
+        }
+    }
+
+    cout << "No repeating character found." << endl;
+
+    return 0;
+}
---
+++ day- 23/max-occuring-character.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string str;
+    
+    cout << "Enter a string: ";
+    getline(cin, str);
+
+    int freq[256] = {0};
+
+    
+    for (int i = 0; i < str.length(); i++) {
+        freq[(int)str[i]]++;
+    }
+
+    char maxChar;
+    int maxFreq = 0;
+
+   
+    for (int i = 0; i < str.length(); i++) {
+        if (freq[(int)str[i]] > maxFreq) {
+            maxFreq = freq[(int)str[i]];
+            maxChar = str[i];
+        }
+    }
+
+    cout << "Maximum occurring character: " << maxChar << endl;
+    cout << "Frequency: " << maxFreq << endl;
+
+    return 0;
+}
